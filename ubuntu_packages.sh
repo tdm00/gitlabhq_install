204cc6ffa5ee0b76ddaf9b5abf73dbc6cfb8ab63
Troy Murray <troy.murray@gmail.com>
2011-12-22 10:50:13 -0500

    add description block for configuring git global options

--- ubuntu_packages.sh
+++ ubuntu_packages.sh
@@ -1,4 +1,7 @@
 # install all packages
 sudo apt-get install git-core wget curl gcc checkinstall libxml2-dev libxslt-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libc6-dev libssl-dev libmysql++-dev make build-essential zlib1g-dev -y
+
+# configure git global options to prevent problems
+# with gitlabhq user pushing repos back to gitolite
 git config --global user.email "admin@local.host"
 git config --global user.name "GitLabHQ Admin User"
