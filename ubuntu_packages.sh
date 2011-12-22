732d063c2f6be1cdaa5e0c967aa3196dd8c5e27a
Troy Murray <troy.murray@gmail.com>
2011-12-22 11:17:31 -0500

    add nano package

--- ubuntu_packages.sh
+++ ubuntu_packages.sh
@@ -1,5 +1,5 @@
 # install all packages
-sudo apt-get install git-core wget curl gcc checkinstall libxml2-dev libxslt-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libc6-dev libpcre3-dev  libssl-dev libmysql++-dev make build-essential zlib1g-dev -y
+sudo apt-get install git-core wget curl gcc checkinstall libxml2-dev libxslt-dev nano sqlite3 libsqlite3-dev libcurl4-openssl-dev libc6-dev libpcre3-dev  libssl-dev libmysql++-dev make build-essential zlib1g-dev -y
 
 # configure git global options to prevent problems
 # with gitlabhq user pushing repos back to gitolite
