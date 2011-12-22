acfc953467ace33933fd18e7f5f860b63825aa32
Troy Murray <troy.murray@gmail.com>
2011-12-22 10:49:22 -0500

    add -y option to apt-get command

--- ubuntu_packages.sh
+++ ubuntu_packages.sh
@@ -1,4 +1,4 @@
 # install all packages
-sudo apt-get install git-core wget curl gcc checkinstall libxml2-dev libxslt-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libc6-dev libssl-dev libmysql++-dev make build-essential zlib1g-dev
+sudo apt-get install git-core wget curl gcc checkinstall libxml2-dev libxslt-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libc6-dev libssl-dev libmysql++-dev make build-essential zlib1g-dev -y
 git config --global user.email "admin@local.host"
 git config --global user.name "GitLabHQ Admin User"
